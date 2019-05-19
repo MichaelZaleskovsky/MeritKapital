2e9e554a8f3fd0d1749d46f939baa72c6d61db24
Mikhail Zaleskovskiy <mz.java.developer@gmail.com>
2019-05-19 15:11:29 +0200

    FrontEnd client

--- MeritKapital/src/main/java/com/meritkapital/MeritKapitalApplication.java
+++ MeritKapital/src/main/java/com/meritkapital/MeritKapitalApplication.java
@@ -1,8 +1,5 @@
 package com.meritkapital;
 
-import java.util.Calendar;
-import java.util.Date;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
